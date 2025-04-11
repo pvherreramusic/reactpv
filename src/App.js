768ea5956d88fbd0c73f85e010fe85cc3a20724d
Patrick-Vincent Herrera <59979695+pvherreramusic@users.noreply.github.com>
2025-04-10 21:50:50 -0700

    MUSIC player added

--- src/App.js
+++ src/App.js
@@ -2,35 +2,37 @@
 import React from "react";
 import "./App.css";
 import xanxacover from "./images/cover2.jpg";
-import {DownloadLinkXanax } from "./DownloadLinkXanax";
-import cigarettescover from "./images/cover.jpg"
+import { DownloadLinkXanax } from "./DownloadLinkXanax";
+import cigarettescover from "./images/cover.jpg";
 import { DownloadLinkCigarettes } from "./DownloadCigarettes";
-import i  from "./images/i.jpg"
 import YoutubeEmbed from "./YoutubeEmbed";
+import XanaxPlayer from "./MusicPlayerXanax";
+import CigPlayer from "./MusicPlayerCig";
 
 function App() {
   const XanaxandMercydownload =
     "https://pvherreramusicfiles.s3.us-east-1.amazonaws.com/Xanax+and+Mercy+MP3.zip";
-     const CigarettesandWeddingsdownload = "https://pvherreramusicfiles.s3.us-east-1.amazonaws.com/Cigarettes+and+Weddings.zip"
-     
+  const CigarettesandWeddingsdownload =
+    "https://pvherreramusicfiles.s3.us-east-1.amazonaws.com/Cigarettes+and+Weddings.zip";
+
   return (
     <div className="App">
       <header className="app-header">
         <h1>Welcome to PV HERRERA MUSIC</h1>
-        <img src = {i} />
+       
         <p>Listen to more music by PV!</p>
       </header>
       <main className="app-main">
         <section className="features-section">
-        <h2>Featured Video</h2>
-            <div className="feature">
-              <h3>"Crystal Clearwater youtube video"</h3>
-              <YoutubeEmbed embedId="xl4MksTBR6I" />
-              
-            </div>
+          <h2>Featured Video</h2>
+          <div className="feature">
+            <h3>"Crystal Clearwater youtube video"</h3>
+            <YoutubeEmbed embedId="xl4MksTBR6I" />
+          </div>
           <h2>Complete Discography</h2>
           <div className="features-grid">
             <div className="feature">
+            <XanaxPlayer />
               <h3>Xanax and Mercy LP 2 Instruemental Rock</h3>
               <img src={xanxacover} alt="Xanax and Mercy Cover" />
               <DownloadLinkXanax
@@ -39,13 +41,14 @@ function App() {
               />
             </div>
             <div className="features-grid">
-            <div className="feature">
-              <h3>Cigarettes and Weddings LP 1 Ambient Post-Rock</h3>
-              <img src={cigarettescover} alt="Cigarettes and Weddings" />
-              <DownloadLinkCigarettes
-                url={CigarettesandWeddingsdownload}
-                fileName="/Cigarettes+and+Weddings.zip"
-              />
+              <div className="feature">
+              <CigPlayer />
+                <h3>Cigarettes and Weddings LP 1 Ambient Post-Rock</h3>
+                <img src={cigarettescover} alt="Cigarettes and Weddings" />
+                <DownloadLinkCigarettes
+                  url={CigarettesandWeddingsdownload}
+                  fileName="/Cigarettes+and+Weddings.zip"
+                />
               </div>
             </div>
           </div>
@@ -53,14 +56,15 @@ function App() {
         <section className="about-section">
           <h2>About PV HERRERA</h2>
           <p>
-            PV Herrera has been active in the music scene of the Central Coast, CA
-            from 2008 to Present. Originally from Los Gatos, he moved to the
+            PV Herrera has been active in the music scene of the Central Coast,
+            CA from 2008 to Present. Originally from Los Gatos, he moved to the
             central coast for better health due to his restricted air way. He
             was given guitar lessons at the age of 13 at a small music lesson
             school. His first project was called The Danger Girl Starship where
             he wrote the majority of his work, starting in 2008. Under his
             personal stage name, he has released two albums. Those albums are:
-            "Cigarettes and Weddings" and "Xanax and Mercy". Download those works for free on this site.
+            "Cigarettes and Weddings" and "Xanax and Mercy". Download those
+            works for free on this site.
           </p>
         </section>
         <section className="contact-section">
